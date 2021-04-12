03c09ab77ff61767290f9520cca5e4a1108d1bc5
TonyJem <a.jemanov@gmail.com>
2021-04-12 19:07:24 +0300

    Start adding UI elements to code

--- UIReminderWithNoStoryboard/UIReminderWithNoStoryboard/MainViewController.swift
+++ UIReminderWithNoStoryboard/UIReminderWithNoStoryboard/MainViewController.swift
@@ -2,7 +2,20 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    private lazy var editButton = {
+        UIBarButtonItem(title: "Edit",
+                        style: .plain,
+                        target: nil,
+                        action: nil
+        )
+    }()
+    
+    private lazy var searchBar = {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
 }
